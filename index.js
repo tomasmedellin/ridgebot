6a43bc8dc51d204b86092efaf90a8c6414f2d187
tomas_medellin <tomasmedellinvelasco@gmail.com>
2025-07-06 00:42:01 -0700

    go go

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers
     ]
 });
 
